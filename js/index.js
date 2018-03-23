695062d4fb7c7f7ecf145080aa65806b5db3892f
vince.su <vacvin@gmail.com>
2018-03-23 17:35:05 +0800

    Rewrite all functions

--- js/index.js
+++ js/index.js
@@ -9,99 +9,129 @@ Ext.onReady(function() {
 })
 
 appInit = function(){
-	Ext.define('Employees', {
-	    extend: 'Ext.data.Store',
-	    alias: 'store.employees',
+	var initTreePanel = function(config) {
+		this.tree = Ext.create('Ext.tree.Panel', {
+            rootVisible: true,
+            width: 280,
+            height: 500,
+            border: false,
+            margin: 10
+        });
 
-	    data: [{
-	        "firstName": "Jean",
-	        "lastName": "Grey",
-	        "officeLocation": "Lawrence, KS",
-	        "phoneNumber": "(372) 792-6728"
-	    }, {
-	        "firstName": "Phillip",
-	        "lastName": "Fry",
-	        "officeLocation": "Lawrence, KS",
-	        "phoneNumber": "(318) 224-8644"
-	    }, {
-	        "firstName": "Peter",
-	        "lastName": "Quill",
-	        "officeLocation": "Redwood City, CA",
-	        "phoneNumber": "(718) 480-8560"
-	    }]
-	});
+		this.panel = Ext.create('Ext.panel.Panel', {
+			region: 'west',
+		    width: 300,
+		    title: 'Tree Panel',	
+		    items: [this.tree]
+		});
+	};
 
-	var tabPage1 = {
-	    xtype: 'grid',
-	    title: 'Employee Directory',
-	    iconCls: 'x-fa fa-users',
-	    layout: 'fit',
-    	fullscreen: true,
-        store: {
-            type: 'employees'
-        },
-        columns: [{
-            text: 'First Name',
-            dataIndex: 'firstName',
-            flex: 1
-        }, {
-            text: 'Last Name',
-            dataIndex: 'lastName',
-            flex: 1
-        }, {
-            text: 'Phone Number',
-            dataIndex: 'phoneNumber',
-            flex: 1
-        }]
-    };
+	var initUserGridPanel = function(config) {
+		this.date = [{
+				        "firstName": "Jean",
+				        "lastName": "Grey",
+				        "officeLocation": "Lawrence, KS",
+				        "phoneNumber": "(372) 792-6728"
+				    }, {
+				        "firstName": "Phillip",
+				        "lastName": "Fry",
+				        "officeLocation": "Lawrence, KS",
+				        "phoneNumber": "(318) 224-8644"
+				    }, {
+				        "firstName": "Peter",
+				        "lastName": "Quill",
+				        "officeLocation": "Redwood City, CA",
+				        "phoneNumber": "(718) 480-8560"
+				    }];
 
-    var tabPage2 = {
-	    title: 'About Sencha',
-        iconCls: 'x-fa fa-info-circle',
-        html: 'Sencha Info Page'
-    };
+		this.store = Ext.create('Ext.data.Store', {
+		    data: this.date
+		});
 
-	var contentPage_top= {
-        region: 'north',        
-        layout: 'form',
-        title: "Employee Input Area",    	
-    	height: 300,
-    	defaultType: 'textfield',
-	    items: [{
-	       fieldLabel: 'First Name',
-	        name: 'first',
-	        allowBlank:false
-	    },{
-	        fieldLabel: 'Last Name',
-	        name: 'last'
-	    },{
-	        fieldLabel: 'Phone Number',
-	        name: 'phoneNumber'
-	    }]
-    };
+		this.panel = Ext.create('Ext.grid.Panel', {
+		    title: 'Employee Directory',
+		    iconCls: 'x-fa fa-users',
+		    layout: 'fit',
+	    	fullscreen: true,
+	        store: this.store,
+	        columns: [{
+	            text: 'First Name',
+	            dataIndex: 'firstName',
+	            flex: 1
+	        }, {
+	            text: 'Last Name',
+	            dataIndex: 'lastName',
+	            flex: 1
+	        }, {
+	            text: 'Phone Number',
+	            dataIndex: 'phoneNumber',
+	            flex: 1
+	        }]
+		});
+	};
 
-    var contentPage_center= {
-        region: 'center',
-        xtype: 'tabpanel',
-		items: [tabPage1, tabPage2]
-    };
+	var initTabInfoPanel = function(config) {		
+		this.panel = Ext.create('Ext.panel.Panel', {
+			title: 'About Sencha',
+	        iconCls: 'x-fa fa-info-circle',
+	        html: 'Sencha Info Page'
+		});
+	};
 
-	var mainPage_west = {
-        region: 'west',        
-        layout: 'form',
-        title: 'Tree panel',
-        width: 300
-    };
+	var initContentTopPanel = function(config) {
+		this.panel = Ext.create('Ext.panel.Panel', {
+			region: 'north',        
+	        layout: 'form',
+	        title: "Employee Input Area",    	
+	    	height: 300,	    	
+	    	defaultType: 'textfield',
+		    items: [{
+		       	fieldLabel: 'First Name',
+		        name: 'first',
+		        allowBlank:false
+		    },{
+		        fieldLabel: 'Last Name',
+		        name: 'last'
+		    },{
+		        fieldLabel: 'Phone Number',
+		        name: 'phoneNumber'
+		    }]
+		});
+	};
 
-    var mainPage_center = {
-        region: 'center',
-        layout: 'border',
-		items: [contentPage_top, contentPage_center]
-    };
+	var initContentCenterPanel = function(config) {
+		this.userGridPanel = new initUserGridPanel(config);
+		this.tabInfoPanel = new initTabInfoPanel(config);
 
-	Ext.create('Ext.container.Viewport', {
-		layout: 'border',
-        title: 'Ext Layout Browser',
-		items: [mainPage_west, mainPage_center]
-	});
+		this.panel = Ext.create('Ext.tab.Panel', {
+			region: 'center',
+	        xtype: 'tabpanel',
+			items: [this.userGridPanel.panel, this.tabInfoPanel.panel]
+		});
+	};
+
+	var initContentPanel = function(config) {
+		this.contentTopPanel = new initContentTopPanel(config);
+		this.contentCenterPanel = new initContentCenterPanel(config);
+
+		this.panel = Ext.create('Ext.panel.Panel', {
+			region: 'center',
+	        layout: 'border',
+	        items: [this.contentTopPanel.panel, this.contentCenterPanel.panel]
+		});
+
+	};
+
+	var initContainer = function(config) {
+		this.treePanel = new initTreePanel(config);
+		this.contentPanel = new initContentPanel(config);
+
+		this.container = Ext.create('Ext.container.Viewport', {
+			layout: 'border',
+	        title: 'Ext Layout Browser',        
+			items: [this.treePanel.panel, this.contentPanel.panel]
+		});
+	};
+
+	new initContainer();
 }
